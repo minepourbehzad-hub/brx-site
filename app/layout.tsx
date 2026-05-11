01bdf3f8f525d703071a8b54fee83626530b40a3
BRX <brx@BRXs-MacBook-Air.local>
2026-05-11 11:03:06 -0700

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script"; 
 import { Inter } from "next/font/google";
 import "./globals.css";
 import SiteHeader from "@/app/components/SiteHeader";
@@ -21,11 +22,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <SiteHeader />
-        {children}
-        <SiteFooter />
-      </body>
+     <body className={inter.className}>
+  <SiteHeader />
+  {children}
+  <SiteFooter />
+
+  <Script
+    src="https://www.googletagmanager.com/gtag/js?id=G-Y5KZ8TZXYW"
+    strategy="afterInteractive"
+  />
+
+  <Script id="google-analytics" strategy="afterInteractive">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-Y5KZ8TZXYW');
+    `}
+  </Script>
+</body>
     </html>
   );
 }
